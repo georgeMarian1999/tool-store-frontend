e38a50448437cbab5d8c0c0b873cdec34855a53b
George <George@GEORGE-PC>
2023-02-02 22:35:39 +0200

    Final version 2

--- src/views/ProductsEdit.vue
+++ src/views/ProductsEdit.vue
@@ -10,10 +10,20 @@
 <script>
 import PageIntro from "@/components/common/PageIntro.vue";
 import ProductEditList from "@/components/products-edit/ProductEditList.vue";
+import axios from "axios";
 
 export default {
   name: "ProductsEdit",
-  components: {ProductEditList, PageIntro}
+  components: {ProductEditList, PageIntro},
+
+  mounted() {
+    let testDeadLockURL1 = "http://localhost:8080/product/test-deadlock/2/3";
+    let testDeadLockURL2 = "http://localhost:8080/product/test-deadlock/3/2";
+
+
+    axios.put(testDeadLockURL1);
+    axios.put(testDeadLockURL2);
+  }
 }
 </script>
 
